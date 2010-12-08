51e316f7ee0f39cde06afc3c78044be326f73977
Yuya Kato <yuyakato@gmail.com>
2010-12-09 01:27:16 +0900

    ファイル名の生成方法を変更

--- plugin/techon.nikkeibp.co.jp/generator.rb
+++ plugin/techon.nikkeibp.co.jp/generator.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-require "digest/md5"
+require "digest/sha1"
 require File.join(File.dirname(__FILE__), "parser")
 require File.join(File.dirname(__FILE__), "formatter")
 
@@ -12,18 +12,19 @@ module TechOn
       article = Parser.extract(src, curl)
 
       article[:images].each { |image|
-        filename, type =
-          case image[:url]
-          when /\.jpg$/i then [Digest::MD5.hexdigest(image[:url]) + ".jpg", "image/jpeg"]
+        image_url = image[:url]
+        ext, type =
+          case image_url
+          when /\.jpg$/i then ["jpg", "image/jpeg"]
           else raise("unknown type")
           end
-        image[:filebody] = http.get(image[:url])
-        image[:filename] = filename
+        image[:filebody] = http.get(image_url)
+        image[:filename] = self.create_filename(image_url, ext)
         image[:filetype] = type
       }
 
       article[:filebody] = Formatter.format(article)
-      article[:filename] = Digest::MD5.hexdigest(article[:url]) + ".xhtml"
+      article[:filename] = self.create_filename(article[:url], "xhtml")
       article[:filetype] = "application/xhtml+xml"
 
       return article
@@ -32,6 +33,10 @@ module TechOn
     def self.get_canonical_url(http, url)
       return $1 if /\A(.+)\?ref=rss\z/ =~ url
       return url
+    end
+
+    def self.create_filename(url, ext)
+      return "techon_#{Digest::SHA1.hexdigest(url)[0, 20]}.#{ext}"
     end
   end
 end
