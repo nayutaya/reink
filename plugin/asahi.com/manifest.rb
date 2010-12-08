e3e57c83f1d681b1c68654eef09ce78cf4ee0652
Yuya Kato <yuyakato@gmail.com>
2010-12-09 01:09:15 +0900

    コーディングスタイルを変更

--- plugin/asahi.com/manifest.rb
+++ plugin/asahi.com/manifest.rb
@@ -3,7 +3,7 @@
 Reink::Plugin::Plugins << {
   :url_pattern => %r|http://www\.asahi\.com/.+|,
   :generator   => proc {
-    require File.join(File.dirname(__FILE__), "generator")
+    require(File.join(File.dirname(__FILE__), "generator"))
     Asahi::Generator
   },
 }
--- plugin/slashdot.jp/manifest.rb
+++ plugin/slashdot.jp/manifest.rb
@@ -3,7 +3,7 @@
 Reink::Plugin::Plugins << {
   :url_pattern => %r|http://slashdot\.jp/.+|,
   :generator   => proc {
-    require File.join(File.dirname(__FILE__), "generator")
+    require(File.join(File.dirname(__FILE__), "generator"))
     Slashdot::Generator
   },
 }
