02df3d421198f8d95f96958ee5f7fd7b3c16f42c
Yuya Kato <yuyakato@gmail.com>
2010-12-08 22:48:06 +0900

    コマンドライン引数の解析処理を追加

---
+++ lib/version.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+module Reink
+  VERSION = "0.0.0"
+end
--- lib/xhtml_command.rb
+++ lib/xhtml_command.rb
@@ -1,15 +1,27 @@
 # coding: utf-8
 
+require "optparse"
 require "rubygems"
 require "log4r"
+require File.join(File.dirname(__FILE__), "version")
 require File.join(File.dirname(__FILE__), "http", "factory")
 require File.join(File.dirname(__FILE__), "http", "message_pack_store")
 require File.join(File.dirname(__FILE__), "..", "plugin", "plugin")
 
 module Reink
   module XhtmlCommand
+    LOG_LEVELS = [:off, :fatal, :error, :warn, :info, :debug]
+
     def self.main(argv)
-      logger = self.create_logger
+      params = {:log_level => :info}
+      opts = OptionParser.new
+      opts.version = Reink::VERSION
+      opts.on("-l", "--log-level LEVEL", LOG_LEVELS) { |v| params[:log_level] = v }
+      opts.parse!(argv)
+
+      p params
+
+      logger = self.create_logger(params[:log_level])
       http   = self.create_http_client(logger)
 
       #p Reink::Plugin::Plugins
@@ -20,7 +32,10 @@ module Reink
       plugin = Reink::Plugin::Plugins.find { |params| params[:url_pattern] =~ url }
       article = plugin[:generator].call(logger, http, url)
 
-      STDOUT.write(article[:filebody])
+#      STDOUT.write(article[:filebody])
+    rescue OptionParser::InvalidArgument => e
+      name = File.basename($0, ".rb")
+      STDERR.puts("#{name}: #{e.message}")
     end
 
     def self.create_http_client(logger)
@@ -31,12 +46,20 @@ module Reink
         :store    => store)
     end
 
-    def self.create_logger
+    def self.create_logger(log_level)
       formatter = Log4r::PatternFormatter.new(:pattern => "%d [%l] %M", :date_pattern => "%H:%M:%S")
       outputter = Log4r::StderrOutputter.new("", :formatter => formatter)
       logger = Log4r::Logger.new($0)
       logger.add(outputter)
-      logger.level = Log4r::DEBUG
+      logger.level =
+        case log_level
+        when :off   then Log4r::OFF
+        when :fatal then Log4r::FATAL
+        when :error then Log4r::ERROR
+        when :warn  then Log4r::WARN
+        when :info  then Log4r::INFO
+        when :debug then Log4r::DEBUG
+        end
       return logger
     end
   end
