6bf5c675b5da1020d089407bc4711405f8079c3a
Yuya Kato <yuyakato@gmail.com>
2010-12-22 02:21:03 +0900

    container.xmlファイルの生成処理を変更

--- lib/epub/container_xml.rb
+++ lib/epub/container_xml.rb
@@ -4,6 +4,7 @@ module Reink
   module Epub
     class ContainerXml
       def to_s
+        return File.open(File.join(File.dirname(__FILE__), "container.xml"), "rb") { |file| file.read }
         return DATA.read
       end
     end
@@ -14,11 +15,3 @@ if $0 == __FILE__
   container_xml = Reink::Epub::ContainerXml.new
   puts(container_xml.to_s)
 end
-
-__END__
-<?xml version="1.0" encoding="UTF-8"?>
-<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">
- <rootfiles>
-  <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
- </rootfiles>
-</container>
