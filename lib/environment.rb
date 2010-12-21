21e2f332483428461625b63199dec0a66b77c193
Yuya Kato <yuyakato@gmail.com>
2010-12-21 10:58:58 +0900

    那由多屋ベースディレクトリ、reinkベースディレクトリ、キャッシュディレクトリを取得する処理を追加

--- lib/environment.rb
+++ lib/environment.rb
@@ -5,15 +5,37 @@ module Reink
     def self.get_home_dir
       case RUBY_PLATFORM
       when /win32/
-        path = ENV["HOMEDRIVE"] + ENV["HOMEPATH"]
-        return File.expand_path(path)
+        dir = ENV["HOMEDRIVE"] + ENV["HOMEPATH"]
+        raise("home not found") unless File.directory?(dir)
+        return File.expand_path(dir)
       else
         raise("unknown platform")
       end
+    end
+
+    def self.get_nayutaya_base_dir
+      dir = File.join(self.get_home_dir, ".nayutaya")
+      Dir.mkdir(dir) unless File.exist?(dir)
+      return dir
+    end
+
+    def self.get_base_dir
+      dir = File.join(self.get_nayutaya_base_dir, "reink")
+      Dir.mkdir(dir) unless File.exist?(dir)
+      return dir
+    end
+
+    def self.get_cache_dir
+      dir = File.join(self.get_base_dir, "cache")
+      Dir.mkdir(dir) unless File.exist?(dir)
+      return dir
     end
   end
 end
 
 if $0 == __FILE__
   p Reink::Environment.get_home_dir
+  p Reink::Environment.get_nayutaya_base_dir
+  p Reink::Environment.get_base_dir
+  p Reink::Environment.get_cache_dir
 end
