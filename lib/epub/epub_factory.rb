97a6b9a626e58605b4590496655aff25c46fd7d1
Yuya Kato <yuyakato@gmail.com>
2010-12-23 14:55:19 +0900

    content.opfの生成処理を追加

--- lib/epub/epub_factory.rb
+++ lib/epub/epub_factory.rb
@@ -19,10 +19,10 @@ module Reink
 
       def create_content_opf(meta, articles)
         content_opf = ContentOpf.new
-        content_opf.uuid      = "UUID"
-        content_opf.title     = "TITLE"
-        content_opf.author    = "AUTHOR"
-        content_opf.publisher = "PUBLISHER"
+        content_opf.uuid      = meta[:uuid]      || raise(ArgumentError, "uuid")
+        content_opf.title     = meta[:title]     || raise(ArgumentError, "title")
+        content_opf.author    = meta[:author]    || raise(ArgumentError, "author")
+        content_opf.publisher = meta[:publisher] || nil
         content_opf.items    << {:id => "ID1", :href => "HREF1", :type => "TYPE1"}
         content_opf.items    << {:id => "ID2", :href => "HREF2", :type => "TYPE2"}
         content_opf.itemrefs << {:idref => "IDREF1"}
@@ -51,7 +51,12 @@ module Reink
 end
 
 if $0 == __FILE__
-  meta     = {}
+  meta     = {
+    :uuid      => "META-UUID",
+    :title     => "META-TITLE",
+    :author    => "META-AUTHOR",
+    :publisher => "META-PUBLISHER",
+  }
   articles = []
 
   factory       = Reink::Epub::EpubFactory.new
@@ -61,8 +66,8 @@ if $0 == __FILE__
   toc_ncx       = factory.create_toc_ncx(meta, articles)
   toc_xhtml     = factory.create_toc_xhtml(meta, articles)
 
-  puts "---", mimetype
-  puts "---", container_xml
+  #puts "---", mimetype
+  #puts "---", container_xml
   puts "---", content_opf
   puts "---", toc_ncx
   puts "---", toc_xhtml
