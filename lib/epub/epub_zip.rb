68e9af9dcb0d6c2040b76ea95f0ff6b3181a6c65
Yuya Kato <yuyakato@gmail.com>
2010-12-23 15:19:20 +0900

    EpubZipクラスを追加

---
+++ lib/epub/epub_zip.rb
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+require "rubygems"
+require "zip/zip"
+
+module Reink
+  module Epub
+    class EpubZip
+      def initialize
+        @entries = []
+      end
+
+      def add(filename, content)
+        @entries << {:filename => filename, :content => content}
+      end
+
+      def write(filename)
+        File.unlink(filename) if File.exist?(filename)
+        Zip::ZipFile.open(filename, Zip::ZipFile::CREATE) { |zip|
+          @entries.each { |entry|
+            zip.get_output_stream(entry[:filename]) { |io|
+              io.write(entry[:content])
+            }
+          }
+        }
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  zip = Reink::Epub::EpubZip.new
+  zip.add("a.txt", "aaa")
+  zip.add("b.txt", "bbb")
+  zip.add("dir/c.txt", "ccc")
+  zip.write("tmp.zip")
+  p zip
+end
