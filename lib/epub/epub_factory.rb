af83110a627548946ce94a7ac5fe123cc73a32b8
Yuya Kato <yuyakato@gmail.com>
2010-12-23 14:45:41 +0900

    epubの要素を生成する処理のひな形を追加

--- lib/epub/epub_factory.rb
+++ lib/epub/epub_factory.rb
@@ -1,13 +1,47 @@
 # coding: utf-8
 
+require File.join(File.dirname(__FILE__), "mimetype")
+require File.join(File.dirname(__FILE__), "container_xml")
+require File.join(File.dirname(__FILE__), "content_opf")
+require File.join(File.dirname(__FILE__), "toc_ncx")
+require File.join(File.dirname(__FILE__), "toc_xhtml")
+
 module Reink
   module Epub
     class EpubFactory
+      def create_mimetype
+        return MimeType.new
+      end
+
+      def create_container_xml
+        return ContainerXml.new
+      end
+
+      def create_content_opf(articles)
+        return ContentOpf.new
+      end
+
+      def create_toc_ncx(articles)
+        return TocNcx.new
+      end
+
+      def create_toc_xhtml(articles)
+        return TocXhtml.new
+      end
     end
   end
 end
 
 if $0 == __FILE__
   factory = Reink::Epub::EpubFactory.new
-  p factory
+  puts "---"
+  puts factory.create_mimetype
+  puts "---"
+  puts factory.create_container_xml
+  puts "---"
+  puts factory.create_content_opf(nil)
+  puts "---"
+  puts factory.create_toc_ncx(nil)
+  puts "---"
+  puts factory.create_toc_xhtml(nil)
 end
