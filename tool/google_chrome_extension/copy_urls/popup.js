4daab0e287c683c1582ec396cb282704692f827e
Yuya Kato <yuyakato@gmail.com>
2011-07-04 19:44:41 +0900

    URL一覧の取得処理を追加

--- tool/google_chrome_extension/copy_urls/popup.js
+++ tool/google_chrome_extension/copy_urls/popup.js
@@ -5,5 +5,15 @@ popup.onLoad = function() {
   chrome.windows.getCurrent(function(currentWindow) {
     currentWindowId = currentWindow.id;
     //alert("currentWindowId: " + currentWindowId);
+    chrome.tabs.getAllInWindow(currentWindowId, function(tabs) {
+      //alert("tabs: " + tabs);
+       //document.getElementById("urls")
+      var urls = "";
+      for ( var i = 0, len = tabs.length; i < len; i++ )
+      {
+        urls += tabs[i].url + "\n";
+      }
+      alert(urls);
+    });
   });
 };
