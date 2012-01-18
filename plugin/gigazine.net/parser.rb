6dbd908c2f03b03085a41c83c807f7df75c27558
Yuya Kato <yuyakato@gmail.com>
2012-01-18 21:35:08 +0900

    発行日時の取得処理を追加

--- plugin/gigazine.net/parser.rb
+++ plugin/gigazine.net/parser.rb
@@ -26,27 +26,14 @@ module Gigazine
       return doc.xpath('//*[@id="maincol"]/div/h1[1]/text()').text.strip
     end
 
-=begin
     def self.extract_published_time(src)
-      return self.extract_published_time1(src)
-    rescue ParseError
-      return self.extract_published_time2(src)
-    end
-
-    def self.extract_published_time1(src)
       doc  = Nokogiri.HTML(src)
-      time = doc.xpath('//*[@id="HeadLine"]/div[@class="Utility"]/p[1]/text()').text.strip
-      raise(ParseError, "invalid time") unless /\A(\d+)年(\d+)月(\d+)日(?:(\d+)時(\d+)分)?\z/ =~ time
+      time = doc.xpath('//*[@id="maincol"]/div[@class="content"]/h3[@class="date"]/text()').text.strip
+      raise(ParseError, "invalid time") unless /(\d+)年(\d+)月(\d+)日 (\d+)時(\d+)分/ =~ time
       return Time.local($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i)
     end
 
-    def self.extract_published_time2(src)
-      doc  = Nokogiri.HTML(src)
-      time = doc.xpath('//dl[@id="TopicPath"]/dd[@class="FloatR"]/text()').text.strip
-      raise(ParseError, "invalid time") unless /\A(\d+)年(\d+)月(\d+)日(?:(\d+)時(\d+)分)?\z/ =~ time
-      return Time.local($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i)
-    end
-
+=begin
     def self.extract_images(src, url)
       doc = Nokogiri.HTML(src)
       images = []
