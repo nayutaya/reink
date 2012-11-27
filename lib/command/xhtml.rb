958651524f7add269ce09941dc4bb356d4712c28
Yuya Kato <yuyakato@gmail.com>
2012-11-28 01:25:38 +0900

    xhtmlコマンドのコマンドライン引数でクッキーを渡せるように変更

--- lib/command/xhtml.rb
+++ lib/command/xhtml.rb
@@ -14,7 +14,7 @@ module Reink
       def self.main(argv)
         params = self.parse_options(argv)
         logger = self.create_logger(params[:log_level])
-        http   = self.create_http_client(logger, params[:interval])
+        http   = self.create_http_client(logger, params[:interval], params[:cookie])
         url    = params[:url]
 
         plugin    = Reink::Plugin.find_by_url(url) || raise("no such plugin for #{url}")
@@ -33,6 +33,7 @@ module Reink
           :url       => nil,
           :interval  => 1.0,
           :log_level => :info,
+          :cookie     => nil,
         }
         OptionParser.new { |opt|
           opt.program_name = "reink xhtml"
@@ -40,6 +41,7 @@ module Reink
           # TODO: -c --cache-dir=DIR を追加
           opt.on("-i", "--interval=SECOND", Float)      { |v| params[:interval]  = v }
           opt.on("-l", "--log-level=LEVEL", log_levels) { |v| params[:log_level] = v }
+          opt.on("-c", "--cookie=COOKIE", String)       { |v| params[:cookie]    = v }
           opt.on("-v", "--verbose")                     { |v| params[:log_level] = :debug }
           opt.parse!(argv)
         }
@@ -72,9 +74,13 @@ module Reink
         return logger
       end
 
-      def self.create_http_client(logger, interval)
+      def self.create_http_client(logger, interval, cookie)
+        header = {}
+        header["Cookie"] = cookie if cookie
+
         store = HttpClient::MessagePackStore.new(File.join(File.dirname(__FILE__), "..", "..", "cache"))
         return HttpClient::Factory.create_client(
+          :header   => header,
           :logger   => logger,
           :interval => interval,
           :store    => store)
